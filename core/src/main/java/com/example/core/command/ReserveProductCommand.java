54640735f85fd0c7c19b7c58a144d767d02d6170
thatchon <71114727+thatchon@users.noreply.github.com>
2022-11-08 10:59:27 +0700

    Add files via upload

---
+++ core/src/main/java/com/example/core/command/ReserveProductCommand.java
@@ -0,0 +1,16 @@
+package com.example.core.command;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class ReserveProductCommand {
+
+    @TargetAggregateIdentifier
+    private final String productId;
+    private final int quantity;
+    private final String orderId;
+    private final String userId;
+}
